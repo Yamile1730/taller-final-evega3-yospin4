6b59474487b96dc19eea764314ce5b93dba30942
Yamile1730 <elsyyamilevega@hotmail.com>
2024-08-16 17:27:22 -0500

    menucompleto

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("---------------------------------------------");
+
+        System.out.println("----1.Datos primitivos----           ");
+        System.out.println("----2.String----                     ");
+        System.out.println("----3.Constantes----                 ");
+        System.out.println("----4.Tipo de Operadores----         ");
+        System.out.println("----5.Condicional if else if else----");
+        System.out.println("----6.Condicional Switch----         ");
+        System.out.println("----7.Condicional Ternaria----       ");
+        System.out.println("----8.Bucle Do While----             ");
+        System.out.println("----9.Bucle While----                ");
+        System.out.println("----10.Bucle For----                 ");
+        System.out.println("----0.Salir----                      ");
+
+        int menu = scanner.nextInt();
+
+        switch (menu) {
+            case 1:
+                System.out.println("1.Datos Primitivos");
+                break;
+
+            case 2:
+                System.out.println("2.String");
+                break;
+            default:
+                System.out.println("Número no válido, por favor verifique");
 
         }
     }
