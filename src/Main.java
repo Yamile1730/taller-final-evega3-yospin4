10b3aee8b15f2ef902d76802fd065bf189c93f9d
Yamile1730 <elsyyamilevega@hotmail.com>
2024-08-28 11:40:45 -0500

    pull actualizacion main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+<<<<<<< HEAD
+=======
 
 public class Main {
     public static void main(String[] args) {
@@ -15,7 +17,38 @@ public class Main {
             System.out.println("2.String");
             System.out.println("3.Constantes");
             System.out.println("4.Salir");
+>>>>>>> eca6daada543c732c330bff45ada946f511b3945
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("---------------------------------------------");
+
+        System.out.println("----1.Datos primitivos----           ");
+        System.out.println("----2.String----                     ");
+        System.out.println("----3.Constantes----                 ");
+        System.out.println("----4.Tipo de Operadores----         ");
+        System.out.println("----5.Condicional if else if else----");
+        System.out.println("----6.Condicional Switch----         ");
+        System.out.println("----7.Condicional Ternaria----       ");
+        System.out.println("----8.Bucle Do While----             ");
+        System.out.println("----9.Bucle While----                ");
+        System.out.println("----10.Bucle For----                 ");
+        System.out.println("----0.Salir----                      ");
+
+        int menu = scanner.nextInt();
+
+        switch (menu) {
+            case 1:
+                System.out.println("1.Datos Primitivos");
+                break;
+
+            case 2:
+                System.out.println("2.String");
+                break;
+            default:
+                System.out.println("Número no válido, por favor verifique");
 
         System.out.println("Seleccione una opcion");
         int nota = scanner.nextInt();
